899d8f9a8f2c9ccb5d05c84297956bb2ec90637d
Dennis <dennis.m.basco@gmail.com>
2018-05-28 00:13:23 +0000

    Added Pos Desc

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,3 +1,7 @@
 class Personnel < ApplicationRecord
   has_many :dependents, :dependent => :delete_all
+
+  def position_description
+  	return Position.where(code: self.pos_code).pluck(:description)
+  end
 end
