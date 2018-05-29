69cde51549462893e9835c522c7835c9927ae1b5
Dennis <dennis.m.basco@gmail.com>
2018-05-29 01:04:02 +0000

    Fixed relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :dependents
-  resources :personnels
+  # resources :dependents
+  resources :personnels do
+  	resources :dependents
+  end
+  
   resources :positions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
