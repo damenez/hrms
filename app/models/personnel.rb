fcb5bdc87ff9a20867fae9597e3dae1ce09772d5
Dennis <dennis.m.basco@gmail.com>
2018-07-03 06:40:57 +0000

    fixes

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,6 +1,7 @@
 class Personnel < ApplicationRecord
   has_many :dependents, :dependent => :delete_all
   validates :name, :presence => true
+  paginates_per 10
 
   def position_description
   	return Position.where(code: self.pos_code).first.description
